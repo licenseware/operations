7939bd0baf724f0a1ad0158f05ef8ba42e630a20
Meysam Azad <meysam@developer-friendly.blog>
2024-11-21 20:27:33 +0700

    chore(email-validator): only business emails are allowed

--- services/email-validator/main.py
+++ services/email-validator/main.py
@@ -33,15 +33,58 @@ async def index():
     )
 
 
-@app.get("/validate")
-async def validate(email: str):
+"""
+Sending the following validation error in case of invalid email:
+
+{
+  "messages": [
+    {
+      "instance_ptr": "#/traits/foo/bar", // Points to the field that failed validation.
+      "messages": [
+        {
+          "id": 123, // Unique numeric ID of the error that helps the frontend to interpret this message, doesn't have to be unique.
+          "text": "field must be longer than 12 characters",
+          "type": "error", // One of "error", "info", "success".
+          "context": {
+            // Contextual information about the error. Is schemaless, and can be used to provide additional information to the frontend.
+            // Typically used to provide fields that are used in internationalization messages.
+            "value": "short value",
+            "any": "additional information"
+          }
+        }
+      ]
+    }
+  ]
+}
+"""
+@app.post("/validate")
+async def validate(request: fastapi.Request):
+    body = await request.json()
+    email = body.get("email")
+
     valid = await is_valid_domain(email) and await is_valid_email(email)
+
+    error_payload = {
+        "messages": [
+            {
+                "instance_ptr": "#/traits/email",
+                "messages": [
+                    {
+                        "id": 1,
+                        "text": "Only business emails are allowed.",
+                        "type": "error",
+                        "context": {"value": email},
+                    }
+                ],
+            }
+        ]
+    }
 
     if valid:
         content = {"valid": True, "msg": "All good."}
         status_code = http.HTTPStatus.OK
     else:
-        content = {"valid": False, "msg": "Non-business email not allowed."}
+        content = error_payload
         status_code = http.HTTPStatus.BAD_REQUEST
 
     return fastapi.responses.JSONResponse(content=content, status_code=status_code)
